817bf9e338df52be79958ae8d95c556136cdf6cc
FrancoRam <franconramirez1@gmail.com>
2025-05-23 19:11:22 -0300

    Primer commit

---
+++ src/primerparcial_ramirezfranco/PrimerParcial_RamirezFranco.java
@@ -0,0 +1,16 @@
+package primerparcial_ramirezfranco;
+
+/**
+ *
+ * @author ThinkPad
+ */
+public class PrimerParcial_RamirezFranco {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
